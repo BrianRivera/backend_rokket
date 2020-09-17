d4021fe7c77679a32ddb806236a359217b5de5ec
brian <brian.rivera.proboste@gmail.com>
2020-09-17 17:20:35 -0300

    server mongoose

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./server/routes/personajes.js'));
 
-let conexion = async() => await mongoose.connect(process.env.URL_DB, {
+let conexion = async() => await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true

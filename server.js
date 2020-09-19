9444c700b5c36e724dcb7fcab9cd2ec9df990698
brian <brian.rivera.proboste@gmail.com>
2020-09-19 15:27:00 -0300

    cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('./config/config');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+app.use(cors())
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
